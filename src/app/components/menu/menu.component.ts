9778c9a6e9138275e3685fe512118913b588d69f
tom-kotlar <tomaskotlar@yahoo.co.uk>
2024-09-22 12:07:25 +0100

    Build(menu.*): Toolbar

---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [MenubarModule, CommonModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss',
+})
+export class MenuComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.items = [
+      { label: '🏠 Home', routerLink: '' },
+      {
+        label: '🧑🏻‍🍳 Angular Cookbook',
+        items: [
+          {
+            label: '🍿 Comunication',
+            items: [
+              {
+                label: '🎬 @Input/Output',
+                routerLink: '/input-output',
+              },
+              {
+                label: '🎬 Service',
+                routerLink: '/service',
+              },
+            ],
+          },
+        ],
+      },
+    ];
+  }
+}
