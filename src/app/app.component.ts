33bc4f8e6932ae594eb5acd56f455f0f3eb6a915
tom-kotlar <tomaskotlar@yahoo.co.uk>
2024-09-22 12:06:58 +0100

    Ref(*): Version upgrade & NPM imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'anglar-projects';
+export class AppComponent implements OnInit {
+  title = 'Angular Projects';
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+  }
 }
